43ddd8f75563230a1578953c15adb92954d83a2c
ZitaBalint <zita.balint1072@gmail.com>
2020-11-18 02:54:59 +0000

    removes map script from the sheet

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,17 +1,4 @@
 
-      function initMap() {
-       
-        const uluru = { lat: 51.4545, lng: 2.5879 };
-      
-        const map = new google.maps.Map(document.getElementById("map"), {
-          zoom: 4,
-          center: uluru,
-        });
-      
-        const marker = new google.maps.Marker({
-          position: uluru,
-          map: map,
-        });
-      }
+     
 
       
